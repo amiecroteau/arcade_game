b0720c00ab27fbeabfa432427e69b36ba16a015a
Amie Croteau <amiecroteau@gmail.com>
2018-07-25 11:55:57 -0400

    Collisions detected and resets games to beginning

--- js/app.js
+++ js/app.js
@@ -1,115 +1,131 @@
-// Enemies our player must avoid
-var Enemy = function(x,y) {
-  	this.x = x;
+// Enemies our player must avoid. creates the bugs and the methods to control them 
+var Enemy = function (x, y) {
+	this.x = x;
 	this.y = y + 55;
-    this.sprite = 'images/enemy-bug.png';
-	this.step=101;
-	this.boundary = this.step*5;
+	this.sprite = 'images/enemy-bug.png';
+	this.step = 101;
+	this.boundary = this.step * 5;
 	this.resetPos = -this.step;
 };
 
 
-Enemy.prototype.update = function(dt) {
-   if(this.x <this.boundary){
-	  this.x += 400 * dt; 
-	  
-   } else{
-	   this.x = this.resetPos;
-   }
+//sets boundarys for the bugs
+Enemy.prototype.update = function (dt) {
+	if (this.x < this.boundary) {
+		this.x += 400 * dt;
+
+	} else {
+		this.x = this.resetPos;
+	}
 };
 
-
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+//renders the bugs
+Enemy.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-/*Enemy.prototype.x = function (x){
-    this.x = x;
-}
-Enemy.prototype.y = function (y){
-    this.y = y;
-}
-*/
- 
-class Hero{
-	constructor(){
+//creates hero and all methods used toc ontrol it
+class Hero {
+	constructor() {
 		//this.x=-15;
 		//this.y=450;
 		this.sprite = 'images/char-cat-girl.png';
 		this.step = 101;
 		this.jump = 83;
 		this.startX = this.step * 2;
-		this.startY = (this.jump * 5)-20;
+		this.startY = (this.jump * 4) + 55;
 		this.x = this.startX;
 		this.y = this.startY;
-		
+		this.victory = false;
+
 	}
-	
+
 	//draw hero on current x and y coordinates
-	render(){
+	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
-	
-	handleInput(input){
-		switch(input){
+//controls key input with hero actions
+	handleInput(input) {
+		switch (input) {
 			case 'left':
-				if (this.x >0){
+				if (this.x > 0) {
 					this.x -= this.step;
 				}
 				break;
 			case 'up':
-				if(this.y>0){
+				if (this.y > 0) {
 					this.y -= this.jump;
 				}
 				break;
 			case 'right':
-				if (this.x < this.step*4){
+				if (this.x < this.step * 4) {
 					this.x += this.step;
 				}
 				break;
 			case 'down':
-				if(this.y <this.jump*4){
+				if (this.y < this.jump * 4) {
 					this.y += this.jump;
 				}
 				break;
 		}
 	}
 
+	update() {
+		//check collisions
+		for (let enemy of allEnemies) {
+			if (this.y === enemy.y && (enemy.x + enemy.step > this.x && enemy.x < this.x + this.step/2)) {
+				this.reset();
+
+			}
+		}
+	
+		//check for winner
+		if (this.y === 55){
+			this.victory = true;
+			
+		}
+		
+		
+		
+	}
+	//resets hero
+	reset(){
+		//sets x and y to the start value of x and y
+		this.y = this.startY;
+		this.x = this.startX;
+	}
+
 }
-
-const player = new Hero();	
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-
-
-
+//Instatiates the player
+const player = new Hero();
+//holds all the enemies in as they are loaded in 
 let allEnemies = [];
 let enemiesYposition = [
-100,
-150,
-200,
+	150,
+	200,
+	250,
 ]
 
-enemiesYposition.forEach(function(y){
-	
-	
-    const bug1 = new Enemy(-101,0, 900);
-	const bug2 = new Enemy((-101*4.5),83, 300);
-	const bug3 = new Enemy ((-101*2.5), 166, 200);
-    
-    allEnemies.push(bug1, bug2, bug3);
+//sets the position for each bug(enemy)
+enemiesYposition.forEach(function (y) {
+
+	const bug1 = new Enemy(-101, 0, 900);
+	const bug2 = new Enemy((-101 * 4.5), 83, 300);
+	const bug3 = new Enemy((-200 * 2.5), 166, 200);
+
+	allEnemies.push(bug1, bug2, bug3);
 
 })
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -13,6 +13,7 @@
  * writing app.js a little simpler to work with.
  */
 
+	
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -22,7 +23,18 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
+        lastTime,
+		id;
+	const modal = document.querySelector('.modal-bg');
+	const replay = document.querySelector('.modal-button');
+	
+	replay.addEventListener('click', function(){
+		modal.classList.toggle('hide');
+		player.reset();
+		player.victory = false;
+		win.requestAnimationFrame(main);
+	});
+	
 
     canvas.width = 505;
     canvas.height = 606;
@@ -55,7 +67,13 @@ var Engine = (function(global) {
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
-        win.requestAnimationFrame(main);
+		
+		if (player.victory ===true){
+			win.cancelAnimationFrame(id);
+			modal.classList.toggle('hide');
+		}else{
+        	id = win.requestAnimationFrame(main);
+		}
     }
 
     /* This function does some initial setup that should only occur once,
@@ -94,7 +112,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
 		//NEED TO DECLARE ENEMY AND PLAYERS FUNCTION FOR THIS TO WORK
-        //player.update();
+        player.update();
     }
 
     /* This function initially draws the "game level", it will then call
